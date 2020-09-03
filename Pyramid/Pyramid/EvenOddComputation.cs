71497ac34883bb45dbcacbb3ff296290845d9018
Sravani6108 <70437744+Sravani6108@users.noreply.github.com>
2020-09-03 16:38:44 +0530

    Add files via upload

---
+++ Pyramid/Pyramid/EvenOddComputation.cs
@@ -0,0 +1,17 @@
+﻿namespace Pyramid
+{
+    static class EvenOddComputation
+    {
+        public static void EvenComputation(int rowVal, int value1, int value2, int i, int j, ref int[,] tri)
+        {
+            if (rowVal % 2 == 0)
+                tri[i, j] = value1 + value2;
+        }
+
+        public static void OddComputation(int rowVal, int value1, int value2, int i, int j, ref int[,] tri)
+        {
+            if (rowVal % 2 != 0)
+                tri[i, j] = value1 + value2;
+        }
+    }
+}
